3b2e92b70242b6724a4a86ff7369715c9ac01773
Neerajkhatri12 <neerajkhatri38@gmail.com>
2024-12-06 11:45:28 +0530

    update

---
+++ training/constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("training/config/config.yaml")
+PARAMS_FILE_PATH = Path("training/config/params.yaml")
+SCHEMA_FILE_PATH = Path("training/config/schema.yaml")
